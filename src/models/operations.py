ec2a18a8a0acd5a68f8c70b394dcec985a6616cb
Ning Wang <ntwang1994@gmail.com>
2019-03-16 04:00:41 -0500

    Adding operations

---
+++ src/models/operations.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class Operation(nn.Module):
+    def __init__(self):
+        super(Operation, self).__init__()
+
+    def forward(self, *input):
+        pass
